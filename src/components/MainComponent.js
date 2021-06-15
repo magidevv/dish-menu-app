a7e9f60cdde6e1c460088f89270adb45bed06eae
Anna B <anna.berejna@gmail.com>
2021-06-16 00:11:55 +0300

    added aboutcomponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -30,7 +31,7 @@ class Main extends Component {
             <Home
                 dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                 promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                leader={this.state.leaders.leaders.filter((leader) => leader.featured)[0]}
             />
         );
     }
@@ -47,6 +48,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path='/home' component={HomePage} />
+            <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
